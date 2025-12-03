d2578eaede399edc21909157df858d21b305b4ca
Ranjit Mali <ranjitmali7677@gmail.com>
2025-12-03 15:05:02 +0530

    Remove Steps

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express=require("express");                  //(1)
+const express=require("express");                  
 const app = express();
-const mongoose= require("mongoose");              //(4)
+const mongoose= require("mongoose");              
 const path=require("path");
 
-// const ExpressError = require("./ExpressError"); //in handling asyn errror(2) file exports
+// const ExpressError = require("./ExpressError"); 
 
-const Chat=require("./Modules/chat.js");           //(7) export module require kiya
-const methodOverride=require("method-override")  ;   //(14 )require method overrirde
+const Chat=require("./Modules/chat.js");           
+const methodOverride=require("method-override")  ;   
 
-app.set("views", path.join(__dirname,"views"));    //(6)
+app.set("views", path.join(__dirname,"views"));   
 app.set("view engine","ejs");
 
-app.use(express.urlencoded({extended:true}));           //(13) to parse data from req.body 
+app.use(express.urlencoded({extended:true}));            
 
-app.use(express.static(path.join(__dirname,"public")));        //(10) add css file
-app.use(methodOverride("_method"));                     // use
+app.use(express.static(path.join(__dirname,"public")));        
+app.use(methodOverride("_method"));                     
 
-main().then( (res) =>{console.log("connection sueccful");              //(5)
+main().then( (res) =>{console.log("connection sueccful");              
 })
 .catch(err => console.log(err));
 
@@ -25,29 +25,13 @@ async function main() {
 
 }
 
-//(8) insert data in Chat madule
 
-// let chat1=new Chat({
-//     from:"neha",
-//     to:"priya",
-//     msg:"send your exam seet",
-//     created_at:new Date()
-// });
 
-// chat1.save().then( (res) =>{
-//     console.log(res);
-// })
-// .catch( (err) =>{
-//     console.log(err);
-// })
+app.post("/chats",(req,res) =>{                     
 
-// Create route.. new chat req  enter in db (12)
+     let {from,to,msg}=req.body;                   
 
-app.post("/chats",(req,res) =>{                     //no need of awit and create async fun 
-
-     let {from,to,msg}=req.body;                   //jaise hi post req aayegi 1st access "from" data 
-
-     let newChat= new Chat({         // create new chat schema from new.ejs
+     let newChat= new Chat({         
       
         from:from,
         msg:msg,
@@ -64,36 +48,17 @@ app.post("/chats",(req,res) =>{                     //no need of awit and create
         console.log(err);
     })
     //  res.render("working");
-    res.redirect("/chats");                          //to return in page
+    res.redirect("/chats");                          
 })
-
-
-// //create new route for Handling sync error in backend 7(error)(3)
-
-// app.get("/chats/:id" , async (req, res, next) =>{
-//     let { id } =req.params;
-//     let chat = await Chat.findById(id);
-//     if(!chat){
-//         throw new ExpressError(404, "Chat Not Found");                //(3) throw new error
-//     }
-//     res.render("edit.js",{ chat });
-
-// });
-
-//Edit route (13) to show edit from
-
 app.get("/chats/:id/edit", async (req, res) =>{
-    let {id}=req.params;                                              //to remove id using params
-    let chat= await Chat.findById(id);                                //id ke basic pr search chat in db ..kuch search krana async function
+    let {id}=req.params;                                              
+    let chat= await Chat.findById(id);                                
     res.render("edit.ejs", { chat });
 });
-
-
 //Update route(14)
-
 app.put("/chats/:id", async (req,res) =>{
-    let {id} =req.params;                                                //id extract
-    let {msg:newMsg} =req.body;                                          //msg extract msg: ko rename kiya newmsg
+    let {id} =req.params;                                                
+    let {msg:newMsg} =req.body;                                          
     console.log(newMsg);
     let updatedChat= await Chat.findByIdAndUpdate(id, {msg: newMsg}, {runvalidators:true , new:true}
 
@@ -111,43 +76,23 @@ app.delete("/chats/:id" , async (req,res) => {
     res.redirect("/chats");
     res.render("app.js");
 })
-
-
-//crete new chat to return the render from  (11)
-
-app.get("/chats/new", (req,res) =>{      //this route return a from in this from we add new chat
+app.get("/chats/new", (req,res) =>{      
     //throw error 
-   // throw new ExpressError(404, "page Not Found");      //(3) throw new error this is backend errror hnadling
+   // throw new ExpressError(404, "page Not Found");      
     res.render("new.ejs")
 })
-
-
-
 //Index route (9)
 
 app.get("/chats",async (req,res) =>{
-    let chats= await Chat.find();                         //async funnction Chat.find()and give data fronm database async fumction await 
+    let chats= await Chat.find();                          
     //console.log(chats); //(15)
     // res.send("woring");
-    res.render("index.ejs", { chats});              //render file ejs then chats pass in ejs file 
+    res.render("index.ejs", { chats});              
 })
-
-
-
 //cerete route
 app.get("/", (req,res) =>{                                     //(3)
  res.send("root is working");
 });
-
-
-//Error handling middleware(4) tghis is Backend error
-// app.use((err,req,res,next)=>{
-//     let {status=500 ,message="Some error Occuerd"}= err;
-//     res.status(status).send(message);
-// })
-
-
-
 app.listen(8080, (res) =>{                                       //(2)
     console.log("server is listioning on port 8080");
 });
